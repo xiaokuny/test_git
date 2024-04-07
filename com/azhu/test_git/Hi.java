476e22508a4f9a20811608207a1c370971d7ee5f
xiaokun <ixiaokun@vip.qq.com>
2024-04-07 17:05:26 +0800

    【初始化项目】hi

---
+++ com/azhu/test_git/Hi.java
@@ -0,0 +1,13 @@
+package com.azhu.test_git;
+
+/**
+ * @author xiaokun
+ * @Classname Hi
+ * @Description TODO
+ * @Date 2024/4/7 17:04
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
